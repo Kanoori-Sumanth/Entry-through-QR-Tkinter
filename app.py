01d1cc137e66120e7adffb62a8d75e4c72c471ac
Sumanth <89911318+Kanoori-Sumanth@users.noreply.github.com>
2022-07-17 12:22:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,30 +17,34 @@ if not os.path.isdir(folName):
 def createQR(idEntry,nameEntry,mobEntry,genWin):
 	global folName
 	text = "%s,%s,%s,"%(idEntry.get(), nameEntry.get(), mobEntry.get())	# would be converted to encrypted form in future
-	text += str(datetime.today().date())
-	file_name = idEntry.get()+".png"
-	file_path = folName + "/" + idEntry.get() + ".png"
+	if(not(len(idEntry.get())==0 or len(nameEntry.get())==0 or len(mobEntry.get())==0)):
+		text += str(datetime.today().date())
+		file_name = idEntry.get()
+		file_name += ".png"
+		file_path = folName + "/" + idEntry.get() + ".png"
 
-	existing = os.listdir(folName)
+		existing = os.listdir(folName)
 
-	if(file_name not in existing):
-		img = qrcode.make(text)	# would be converted to encrypted form in future
-		img.save(file_path)
+		if(file_name not in existing):
+			img = qrcode.make(text)	# would be converted to encrypted form in future
+			img.save(file_path)
 
-		# while(1):
-		# 	cv2.imshow("Take pic",img)
-		# 	if(cv2.waitKey(0) & 0xFF==ord('q')):
-		# 		break
-	
+			# while(1):
+			# 	cv2.imshow("Take pic",img)
+			# 	if(cv2.waitKey(0) & 0xFF==ord('q')):
+			# 		break
+		
 
+		else:
+			messagebox.showwarning("Warning","QR for %s already exists"%(file_name))
+
+		img = cv2.imread(file_path)
+		cv2.imshow("QR code for %s"%(file_name),img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+		# genWin.destroy()
 	else:
-		messagebox.showwarning("Warning","QR for %s already exists"%(file_name))
-
-	img = cv2.imread(file_path)
-	cv2.imshow("QR code for %s"%(file_name),img)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
-	# genWin.destroy()
+		messagebox.showwarning("Warning","Some field missing",master=genWin)
 
 	# sys.exit(0)
 
@@ -48,50 +52,37 @@ def createQR(idEntry,nameEntry,mobEntry,genWin):
 def openGenerator(win):
 	genWin = Tk()
 	genWin.title("Generate QR Code")
-	genWin.geometry("1000x800")
+	genWin.geometry("600x500")
 
-	leftFont = ("Times 20")
+	frameFont = ("Times 20")
 
-	left = Frame(genWin,highlightbackground="black",highlightthickness=2)
-	left.grid(row=0,column=0,padx=20,pady=100)
+	frame = Frame(genWin)
+	frame.grid(row=0,column=0,padx=90,pady=100)
 
-	Label(left,text="Enter student details:",font=("Times 30"),highlightbackground="black", highlightthickness=2).grid(row=0,column=0,columnspan=2,padx=10,pady=10)
+	Label(frame,text="Enter student details",font=("Times 30"),highlightbackground="black", highlightthickness=2).grid(row=0,column=0,columnspan=2,padx=10,pady=10)
 
-	Label(left,text="ID:",font=leftFont,highlightbackground="black", highlightthickness=2).grid(row=1,column=0,padx=10,pady=10)
+	Label(frame,text="ID:",font=frameFont).grid(row=1,column=0,padx=10,pady=10)
 	idVal = StringVar()
-	idEntry = Entry(left,font=leftFont,textvariable=idVal)
+	idEntry = Entry(frame,font=frameFont,textvariable=idVal)
 	idEntry.grid(row=1,column=1)
 	# idEntry.focus_force()
 
-	Label(left,text="Name:",font=leftFont,highlightbackground="black", highlightthickness=2).grid(row=2,column=0,padx=10,pady=10)
+	Label(frame,text="Name:",font=frameFont).grid(row=2,column=0,padx=10,pady=10)
 	nameVal = StringVar()
-	nameEntry = Entry(left,font=leftFont,textvariable=nameVal)
+	nameEntry = Entry(frame,font=frameFont,textvariable=nameVal)
 	nameEntry.grid(row=2,column=1)
 
-	Label(left,text="Mobile no:",font=leftFont,highlightbackground="black", highlightthickness=2).grid(row=3,column=0,padx=10,pady=10)
+	Label(frame,text="Mobile no:",font=frameFont).grid(row=3,column=0,padx=10,pady=10)
 	mobVal = StringVar()
-	mobEntry = Entry(left,font=leftFont,textvariable=mobVal)
+	mobEntry = Entry(frame,font=frameFont,textvariable=mobVal)
 	mobEntry.grid(row=3,column=1)
 
 	# msgVal = StringVar()
-	msgLabel = Label(left,font=leftFont,text="Press 'q' to close QR",highlightbackground="black", highlightthickness=2)
+	msgLabel = Label(frame,font=frameFont,text="Press 'q' to close QR",highlightbackground="black", highlightthickness=2)
 	msgLabel.grid(row=5,column=0,columnspan=2,)
 
-	submitBtn = Button(left,text="Submit",font=leftFont,command=lambda:createQR(idEntry,nameEntry,mobEntry,genWin))
+	submitBtn = Button(frame,text="Submit",font=frameFont,command=lambda:createQR(idEntry,nameEntry,mobEntry,genWin))
 	submitBtn.grid(row=4,column=0,columnspan=2)
-
-
-
-
-	right = Frame(genWin,pady=50)
-	right.grid(row=0,column=1)
-
-	qrImgFrame = Frame(right,highlightbackground="black",highlightthickness=2)
-	qrImgFrame.grid(row=0,column=0)
-
-	# img = ImageTk.PhotoImage(Image.open(file_path),master=qrImgFrame)
-	# imgLabel = Label(qrImgFrame,image=img)
-	# imgLabel.grid(row=0,column=0)
 
 
 
@@ -100,7 +91,7 @@ def openGenerator(win):
 
 
 def openScanner(win):
-	scanWin = Toplevel(win)
+	scanWin = Tk()
 	scanWin.title("Scan QR Code")
 	scanWin.geometry("1000x800")
 
