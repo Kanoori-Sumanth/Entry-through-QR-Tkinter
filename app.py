00099ddffdfa66a80e1673fdd4b7fee0db2be4fb
Sumanth <89911318+Kanoori-Sumanth@users.noreply.github.com>
2022-08-22 22:01:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from classes import MainWin
+from tkinter import *
 
 if __name__ == "__main__":
 	mainWindow = MainWin("Student Entry/Exit through QR","800x500")
+	mainWindow["background"] = "#9fafca"
 	mainWindow.open()
 
 # TODO:
-	# encrypting id in qr
-	# saving scanned data into excel file
+
+	# what if an id is scanned and it is not present in json file.
+	# handle exception while trying to take data of certain id from json file
