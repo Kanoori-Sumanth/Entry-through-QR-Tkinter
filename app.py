ba006abbdd54b30b57df42ca6454f53d343d5e8b
Sumanth <89911318+Kanoori-Sumanth@users.noreply.github.com>
2022-09-12 02:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,3 @@ if __name__ == "__main__":
 	mainWindow = MainWin("Student Entry/Exit through QR","800x500")
 	mainWindow["background"] = "#9fafca"
 	mainWindow.open()
-
-# TODO:
-
-	# what if an id is scanned and it is not present in json file.
-	# handle exception while trying to take data of certain id from json file
